c22acd79457966baf6b3c9561108b68ee6a752e6
trainee <trainee@factweavers.com>
2021-06-22 19:08:52 +0530

    object was not called

--- test_read_elasticsearch.py
+++ test_read_elasticsearch.py
@@ -1,11 +1,17 @@
 import unittest
 from read_elasticsearch import EsToCsv
 import csv
+import os
 
 def test_read_elasticsearch():
+
     index_name = "data"
     csv_filename = "my_data.csv"
+    if os.path.exists(csv_filename):
+        os.remove(csv_filename)
+
     es_csv = EsToCsv(index_name, csv_filename)
+    es_csv.upload_json()
     file = open(csv_filename)
     reader = csv.reader(file)
     lines = len(list(reader))
